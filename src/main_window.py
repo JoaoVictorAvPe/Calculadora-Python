5f9180d6f363f609515496c558e448eba93ed038
JoaoVictorAvPe <joaoavilaperasol@hotmail.com>
2024-09-09 18:50:41 -0300

    Criação da primeira janela

---
+++ src/main_window.py
@@ -0,0 +1,17 @@
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.central_widget = QWidget()
+        self.vbox_layout = QVBoxLayout()
+
+        self.central_widget.setLayout(self.vbox_layout)
+        self.setCentralWidget(self.central_widget)
+        self.setWindowTitle('Calculadora')
+
+    
+    def adjustFixedSize(self) -> None:
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
